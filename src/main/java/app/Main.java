d77ea7231d8c92ca49e1caabe26ff026d801b83f
montif16 <Lukas1532@hotmail.com>
2024-11-21 13:24:48 +0100

    Initial structure setup

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,31 @@
 package app;
 
-public class Main {
-    public static void main(String[] args) {
+import app.config.SessionConfig;
+import app.config.ThymeleafConfig;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
 
+public class Main {
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+
+    public static void main(String[] args) {
+        // Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7072);
+
+        // Routing
+
+        app.get("/", ctx -> ctx.render("index.html"));
     }
 }
--- src/main/java/app/exception/DatabaseException.java
+++ src/main/java/app/exception/DatabaseException.java
@@ -1,4 +1,13 @@
 package app.exception;
 
-public class DatabaseException {
+import java.sql.SQLException;
+
+public class DatabaseException extends Exception {
+    public DatabaseException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public DatabaseException(String errorMessage, SQLException exception) {
+        super(errorMessage, exception);
+    }
 }
