7ce8013a962432e1a9601121bcb36169371d5f25
montif16 <Lukas1532@hotmail.com>
2024-11-21 13:37:22 +0100

    string

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -10,7 +10,7 @@ public class Main {
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "";
+    private static final String DB = "fogdatabase";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
