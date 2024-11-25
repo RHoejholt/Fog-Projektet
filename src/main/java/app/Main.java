8162d387652541fcbb1cbc46e8217de0af1cd841
montif16 <Lukas1532@hotmail.com>
2024-11-25 11:10:45 +0100

    Swapped database from placeholder to correct name

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -10,7 +10,7 @@ public class Main {
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "fogdatabase";
+    private static final String DB = "fog";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
