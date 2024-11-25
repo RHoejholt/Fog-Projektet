416916142b22f72c257aa6503d38c182c1944ab0
montif16 <Lukas1532@hotmail.com>
2024-11-25 11:27:39 +0100

    Initial user logic + PWD hashing + pom.xml import and routing in main

--- src/main/java/app/controller/UserController.java
+++ src/main/java/app/controller/UserController.java
@@ -1,4 +1,41 @@
 package app.controller;
 
+import app.exception.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.UserMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
 public class UserController {
+    public static void addRoutes(Javalin app, ConnectionPool dbConnection) {
+        app.get("/createuser", ctx -> ctx.render("createuser.html"));
+        app.post("/createuser", ctx -> createUser(ctx, dbConnection));
+    }
+
+    private static void createUser(Context ctx, ConnectionPool dbConnection) {
+        String username = ctx.formParam("username");
+        String password = ctx.formParam("password");
+        String confirmPassword = ctx.formParam("confirmpassword");
+        // Simple email check
+        if (username == null || !username.contains("@") || !username.contains(".")) {
+            ctx.attribute("message", "Venligst indtast en gyldig e-mail-adresse.");
+            ctx.render("createuser.html");
+        } else if (password == null || confirmPassword == null) {
+            ctx.attribute("message", "Venligst udfyld dit kodeord i begge felter.");
+            ctx.render("createuser.html");
+            try {
+                username = username.toLowerCase();
+                UserMapper.createUser(username, password, dbConnection);
+            } catch (DatabaseException e) {
+                if (e.getMessage().contains("duplicate key value violates unique constraint")) {
+                    ctx.attribute("message", "Brugernavnet er allerede i brug. Prøv et andet.");
+                } else {
+                    ctx.attribute("message", e.getMessage());
+                }
+                ctx.render("createuser.html");
+            }
+        } else {
+            ctx.render("createuser.html");
+        }
+    }
 }
--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -1,4 +1,29 @@
 package app.persistence;
 
+import app.exception.DatabaseException;
+import org.mindrot.jbcrypt.BCrypt;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserMapper {
+    public static void createUser(String username, String password, ConnectionPool pool) throws DatabaseException
+    {
+        String sql = "insert into users (username, password, role) VALUES (?,?,?);";
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, hashedPassword);
+            ps.setString(3, "customer");
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Fejl ved oprettelse af bruger");
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
 }
