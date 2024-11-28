78ae08c70b6e8398606bbc1d281deae8df103ea5
RHoejholt <158362000+RHoejholt@users.noreply.github.com>
2024-11-29 00:22:29 +0100

    Boilerplate

--- src/main/java/app/entities/Order.java
+++ src/main/java/app/entities/Order.java
@@ -1,4 +1,24 @@
 package app.entities;
 
 public class Order {
+
+
+    private int length;
+    private int width;
+    private int id;
+
+    public Order(int width, int id, int length) {
+        this.width = width;
+        this.id = id;
+        this.length = length;
+    }
+
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
 }
