1d0f060cd1257e370a26a46d32244120c2c64f9c
RHoejholt <158362000+RHoejholt@users.noreply.github.com>
2024-11-29 00:22:19 +0100

    Boilerplate

--- src/main/java/app/entities/OrderItem.java
+++ src/main/java/app/entities/OrderItem.java
@@ -1,4 +1,38 @@
 package app.entities;
 
 public class OrderItem {
+
+    private int orderItemId;
+    private Order order;
+    private ProductVariant productVariant;
+    private int quantity;
+    private String description;
+
+    public OrderItem(int orderItemId, Order order, ProductVariant productVariant, int quantity, String description) {
+        this.orderItemId = orderItemId;
+        this.order = order;
+        this.productVariant = productVariant;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    public int getOrderItemId() {
+        return orderItemId;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public ProductVariant getProductVariant() {
+        return productVariant;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
