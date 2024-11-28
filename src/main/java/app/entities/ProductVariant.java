dd52171d218f13588e652e24aacf560f05cba1ef
RHoejholt <158362000+RHoejholt@users.noreply.github.com>
2024-11-29 00:39:54 +0100

    boilerplate

--- src/main/java/app/entities/ProductVariant.java
+++ src/main/java/app/entities/ProductVariant.java
@@ -1,4 +1,28 @@
 package app.entities;
 
 public class ProductVariant {
+
+        private int productVariantId;
+        private Product product;
+        private int length;
+
+    public ProductVariant(int productVariantId, Product product, int length) {
+        this.productVariantId = productVariantId;
+        this.product = product;
+        this.length = length;
+    }
+
+    public int getProductVariantId() {
+        return productVariantId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+
 }
