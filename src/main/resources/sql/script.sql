e419ff4804f157d57b5f66d4b704f645fe288394
RHoejholt <158362000+RHoejholt@users.noreply.github.com>
2024-12-10 00:04:52 +0100

    Updated to reflect ERD

--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -10,7 +10,9 @@ CREATE TABLE IF NOT EXISTS public.orders
     status character varying(64) NOT NULL,
     user_id integer default NULL,
     PRIMARY KEY (order_id)
-);
+    );
+
+
 
 CREATE TABLE IF NOT EXISTS public.users
 (
@@ -25,7 +27,137 @@ ALTER TABLE IF EXISTS public.orders
     ADD CONSTRAINT fk FOREIGN KEY (user_id)
     REFERENCES public.users (user_id) MATCH SIMPLE
     ON UPDATE NO ACTION
-    ON DELETE NO ACTION
+       ON DELETE NO ACTION
     NOT VALID;
+
+CREATE TABLE IF NOT EXISTS public.tag_materiale (
+                                                    id serial NOT NULL,
+                                                    materiale character varying(64) NOT NULL,
+    tag_type character varying(64) NOT NULL,
+    PRIMARY KEY (id)
+    );
+
+CREATE TABLE IF NOT EXISTS public.dimensioner_bredde
+(
+    bredde_id serial NOT NULL,
+    bredde integer NOT NULL,
+    PRIMARY KEY (bredde_id)
+    );
+
+CREATE TABLE IF NOT EXISTS public.dimensioner_laengde
+(
+    laengde_id serial NOT NULL,
+    laengde integer NOT NULL,
+    PRIMARY KEY (laengde_id)
+    );
+
+CREATE TABLE IF NOT EXISTS public.spaer_og_rem
+(
+    spaer_og_rem_id serial NOT NULL,
+    materiale character varying(64) NOT NULL,
+    PRIMARY KEY (spaer_og_rem_id)
+    );
+
+INSERT INTO public.spaer_og_rem (spaer_og_rem_id, materiale)
+VALUES
+    (1,'Benders sort'),
+    (2,'Benders brun'),
+    (3,'Benders tegldrød'),
+    (4,'Benders rød'),
+    (5,'Eternit grå B6');
+
+-- Table: product
+CREATE TABLE product (
+                         product_id SERIAL PRIMARY KEY,
+                         name VARCHAR(100),
+                         unit VARCHAR,
+                         price INTEGER
+);
+
+-- Table: product_variant
+CREATE TABLE product_variant (
+                                 product_variant_id SERIAL PRIMARY KEY,
+                                 length INTEGER,
+                                 product_id INTEGER REFERENCES product(product_id)
+);
+
+-- Table: order_item
+CREATE TABLE order_item (
+                            order_item_id SERIAL PRIMARY KEY,
+                            order_id INTEGER,
+                            product_variant_id INTEGER REFERENCES product_variant(product_variant_id),
+                            quantity INTEGER,
+                            description VARCHAR(100)
+);
+
+INSERT INTO public.product (product_id, name, unit, price)
+VALUES
+    (1, 'Pillar', '', 10),
+    (2, 'Rafter', '', 10),
+    (3, 'Beam', '', 10);
+
+
+INSERT INTO public.product_variant (product_variant_id, length, product_id)
+VALUES
+    (1, '300',  1),
+    (2, '480', 2),
+    (3, '600', 2),
+    (4, '600', 3);
+
+
+INSERT INTO public.dimensioner_bredde (bredde, bredde_id)
+VALUES
+    (270, 1),
+    (300, 2),
+    (330, 3),
+    (360, 4),
+    (390, 5),
+    (420, 6),
+    (450, 7),
+    (480, 8),
+    (510, 9),
+    (540, 10),
+    (570, 11),
+    (600, 12);
+
+INSERT INTO public.dimensioner_laengde (laengde, laengde_id)
+VALUES
+    (270, 1),
+    (300, 2),
+    (330, 3),
+    (360, 4),
+    (390, 5),
+    (420, 6),
+    (450, 7),
+    (480, 8),
+    (510, 9),
+    (540, 10),
+    (570, 11),
+    (600, 12),
+    (630, 13),
+    (660, 14),
+    (690, 15),
+    (720, 16),
+    (750, 17),
+    (780, 18);
+
+INSERT INTO public.tag_materiale (materiale, tag_type)
+VALUES
+    ('Sunlux 1300K', 'Fladt'),
+    ('Sunlux 1200N', 'Fladt'),
+    ('Benders sort', 'Skrå'),
+    ('Benders brun', 'Skrå'),
+    ('Benders tegldrød', 'Skrå'),
+    ('Benders rød', 'Skrå'),
+    ('Eternit grå B6', 'Skrå');
+
+/*
+INSERT INTO public.skur (id,skur)
+VALUES
+    (1,Ja),
+    (2,Nej);
+*/
+
+
 
 END;
