e5278aba15a228843b00a15d4490f503298654c3
RHoejholt <158362000+RHoejholt@users.noreply.github.com>
2024-12-14 22:10:26 +0100

    calculator comments and logic improval

--- src/main/java/app/services/Calculator.java
+++ src/main/java/app/services/Calculator.java
@@ -13,12 +13,12 @@ import java.util.List;
 public class Calculator {
     //Alle mål i mm.
     private static final int OVERHANG = 1300;
-    private static final int MAX_DIST = 3500;
+    private static final int maxDistanceBetweenPillars = 3500;
     private static final int PILLARID = 1;  //Vi bruger "pillar" da "post" har flere betydninger
     private static final int RAFTERID = 3; //SPÆR
     private static final int BEAMID = 2; //REMME
     private static final int MAX_PLANK_LENGTH = 6000;
-    private static final int RAFTER_SEPARATION_DISTANCE = 550;
+    private static final int maxDistanceBetweenRafters = 550;
 
     private static ConnectionPool connectionPool;
 
@@ -33,8 +33,15 @@ public class Calculator {
 
     // Beregn stolper og remme. Bægge beregner sker i denne metode da det ellers bliver noget rod med unit tests.
     public static int calcPillar(int carportLength)  {
-        int quantity = 2 * (2 + (carportLength*10 - MAX_DIST - OVERHANG) / MAX_DIST);
+        // The whole quantity is first multiplied by 2, since there's two symmetrical sides of the carport.
+        // In the paranthesis, the amount needed for one side is calculated. We need at least 2 in each side for the carport to have at least 4 total.
+        // Then we multiply the ordered carport's length by 10 to convert cm to mm
+        // Then we subtract the overhang since 1,3m of the carport's length will always be overhang that we do not need to account for.
+        // Then we divide by the max seperation distance, and since the result is put into an integer, it will automatically be rounded down
+        // So if the unsupported distance is greater than the maximum allowed, it will round down to at least 1, and be added to quantity
+        int quantity = 2 * (2 + (carportLength*10 - OVERHANG) / maxDistanceBetweenPillars);
 
+        // Here we check if any beam seams are unsupported, and add a pillar to each side for each unsupported beam seam.
         int extraPillars = 0;
         if (carportLength*10 > MAX_PLANK_LENGTH) {
             extraPillars += (carportLength - MAX_PLANK_LENGTH) / MAX_PLANK_LENGTH;
@@ -71,22 +78,23 @@ public class Calculator {
 
     }
 
+    // Beams calculation (Remme)
+    //This does not need a "seperation distance", since the beams do not lay in parallel, but in extension of each other
     public static int calcBeams(int carportLength) {
 
         int quantity = 2;
         if (carportLength*10 > MAX_PLANK_LENGTH) {
             quantity += (carportLength*10 - MAX_PLANK_LENGTH) / MAX_PLANK_LENGTH;
         }
-        quantity += quantity; // Adds extra pillars if needed
 
         return quantity;
     }
 
-    // Rafters calculation
+    // Rafters calculation (Spær)
     public static int calcRafters(int carportWidth) {
 
         // Calculate the number of rafters based on the separation distance
-        int quantityOfRafters = (carportWidth*10 + RAFTER_SEPARATION_DISTANCE - 1) / RAFTER_SEPARATION_DISTANCE; // Rounding up to include the last rafter
+        int quantityOfRafters = (carportWidth*10 + maxDistanceBetweenRafters - 1) / maxDistanceBetweenRafters; // Rounding up to include the last rafter
 
         return quantityOfRafters;
     }
