61a09671114919b8e0306f1ce716eda439ab0daa
RHoejholt <158362000+RHoejholt@users.noreply.github.com>
2024-12-15 20:26:53 +0100

    sql

--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS public.dimensioner_bredde
     bredde integer NOT NULL,
     CONSTRAINT dimensioner_bredde_pkey PRIMARY KEY (bredde_id),
     CONSTRAINT unique_bredde UNIQUE (bredde)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS public.dimensioner_laengde
 (
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS public.dimensioner_laengde
     laengde integer NOT NULL,
     CONSTRAINT dimensioner_laengde_pkey PRIMARY KEY (laengde_id),
     CONSTRAINT uniqe_laengde UNIQUE (laengde)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS public.order_item
 (
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS public.order_item
     quantity integer,
     description character varying(100) COLLATE pg_catalog."default",
     CONSTRAINT order_item_pkey PRIMARY KEY (order_item_id)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS public.orders
 (
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS public.orders
     skur character varying COLLATE pg_catalog."default",
     username character varying COLLATE pg_catalog."default",
     CONSTRAINT orders_pkey PRIMARY KEY (order_id)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS public.product
 (
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS public.product
     unit character varying COLLATE pg_catalog."default",
     price integer,
     CONSTRAINT product_pkey PRIMARY KEY (product_id)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS public.product_variant
 (
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS public.product_variant
     length integer,
     product_id integer,
     CONSTRAINT product_variant_pkey PRIMARY KEY (product_variant_id)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS public.skur
 (
@@ -65,7 +65,7 @@ CREATE TABLE IF NOT EXISTS public.skur
     skur character varying COLLATE pg_catalog."default",
     CONSTRAINT skur_pkey PRIMARY KEY (id),
     CONSTRAINT fk_skur UNIQUE (skur)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS public.spaer_og_rem
 (
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS public.spaer_og_rem
     materiale character varying(64) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT spaer_og_rem_pkey PRIMARY KEY (spaer_og_rem_id),
     CONSTRAINT unique_spaer_og_rem UNIQUE (materiale)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS public.tag_materiale
 (
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS public.tag_materiale
     materiale character varying(64) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT tag_materiale_pkey PRIMARY KEY (id),
     CONSTRAINT unique_tag_materiale UNIQUE (materiale)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS public.users
 (
@@ -91,68 +91,68 @@ CREATE TABLE IF NOT EXISTS public.users
     role character varying(12) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (user_id),
     CONSTRAINT users_username_key UNIQUE (username)
-);
+    );
 
 ALTER TABLE IF EXISTS public.order_item
     ADD CONSTRAINT order_item_product_variant_id_fkey FOREIGN KEY (product_variant_id)
-        REFERENCES public.product_variant (product_variant_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION;
+    REFERENCES public.product_variant (product_variant_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
 
 
 ALTER TABLE IF EXISTS public.orders
     ADD CONSTRAINT fk_bredde FOREIGN KEY (bredde)
-        REFERENCES public.dimensioner_bredde (bredde) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
+    REFERENCES public.dimensioner_bredde (bredde) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION
+    NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.orders
     ADD CONSTRAINT fk_laengde FOREIGN KEY (laengde)
-        REFERENCES public.dimensioner_laengde (laengde) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
+    REFERENCES public.dimensioner_laengde (laengde) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION
+    NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.orders
     ADD CONSTRAINT fk_skur FOREIGN KEY (skur)
-        REFERENCES public.skur (skur) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
+    REFERENCES public.skur (skur) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION
+    NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.orders
     ADD CONSTRAINT fk_spaer_og_rem_materiale FOREIGN KEY (spaer_og_rem_materiale)
-        REFERENCES public.spaer_og_rem (materiale) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
+    REFERENCES public.spaer_og_rem (materiale) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION
+    NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.orders
     ADD CONSTRAINT fk_tag_materiale FOREIGN KEY (tag_materiale)
-        REFERENCES public.tag_materiale (materiale) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
+    REFERENCES public.tag_materiale (materiale) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION
+    NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.orders
     ADD CONSTRAINT fk_username FOREIGN KEY (username)
-        REFERENCES public.users (username) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
+    REFERENCES public.users (username) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION
+    NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.product_variant
     ADD CONSTRAINT product_variant_product_id_fkey FOREIGN KEY (product_id)
-        REFERENCES public.product (product_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION;
+    REFERENCES public.product (product_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
 
 INSERT INTO public.dimensioner_bredde (bredde, bredde_id)
 VALUES
