1807da4efe59f8d2410020f0576d3dd17eb689b8
RHoejholt <158362000+RHoejholt@users.noreply.github.com>
2024-12-17 01:24:09 +0100

    Added jdbc info and ports

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -13,8 +13,8 @@ import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class Main {
     private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String PASSWORD = "datdat2024!!";
+    private static final String URL = "jdbc:postgresql://164.90.237.79:5432/%s?currentSchema=public";
     private static final String DB = "postgres";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
@@ -27,7 +27,7 @@ public class Main {
             config.staticFiles.add("/public");
             config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
-        }).start(7072);
+        }).start(3484);
 
         // Routing
         app.get("/", ctx -> ctx.render("index.html"));
